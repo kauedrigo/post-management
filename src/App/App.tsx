894fbfff1c884d022252b70d465a04938403f9dc
Kaue <kaue.drigo@cybergenios.com.br>
2023-04-23 16:25:31 -0300

    feat: routes config

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,14 @@
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Login } from '../pages'
 
 function App() {
-  return <Login />
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </HashRouter>
+  )
 }
 
 export default App
