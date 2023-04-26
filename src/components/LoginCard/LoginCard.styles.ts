46ca07e3facd86c8197680a94b867d901c93538f
Kaue <kaue.drigo@cybergenios.com.br>
2023-04-25 21:30:04 -0300

    feat: login card mobile responsiveness

--- src/components/LoginCard/LoginCard.styles.ts
+++ src/components/LoginCard/LoginCard.styles.ts
@@ -2,16 +2,23 @@ import styled, { css } from 'styled-components'
 
 export const Container = styled.form`
   ${() => css`
-    width: 500px;
+    width: 100%;
+    max-width: 500px;
     display: flex;
     flex-direction: column;
 
     box-sizing: border-box;
-    padding: 24px;
+    padding: 16px;
     gap: 16px;
+    margin: 12px;
 
     background: #ffffff;
     border: 1px solid #cccccc;
-    border-radius: 16px;
+    border-radius: 12px;
+
+    @media screen and (min-width: 768px) {
+      padding: 24px;
+      border-radius: 16px;
+    }
   `}
 `
